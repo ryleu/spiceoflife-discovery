7fffdb48c521818675b015e5afc3f3888285e4e3
ryleu <69326171+ryleu@users.noreply.github.com>
2025-01-24 14:07:42 -0600

    start work on history screen

--- src/client/java/me/ryleu/soldisco/FoodHistoryScreen.java
+++ src/client/java/me/ryleu/soldisco/FoodHistoryScreen.java
@@ -12,10 +12,12 @@ public class FoodHistoryScreen extends BaseUIModelScreen<FlowLayout> {
 
     @Override
     protected void build(FlowLayout rootComponent) {
+        rootComponent.children().forEach(System.out::println);
         rootComponent.childById(ButtonComponent.class, "the-button").onPress(button -> {
             if (minecraft != null && minecraft.player != null) {
+
                 ((IPlayer) minecraft.player).soldisco$getFoodHistory().iterator().forEachRemaining(item ->
-                        SOLDisco.LOGGER.info("{} has {}", minecraft.player.getDisplayName(), item));
+                        SOLDisco.LOGGER.info("{} has {}", minecraft.player.getName(), item));
             }
 
             System.out.println("click");
